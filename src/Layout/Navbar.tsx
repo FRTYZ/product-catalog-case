0c0f998d0728ba5a53124eed8e09ff32d4b0d2df
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-11-03 01:03:03 +0300

    Edited navbar

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -1,27 +1,86 @@
-import React, {useState, useEffect} from "react";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
 
-// Material UI elements
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Badge
+} from '@mui/material';
+
 import { 
-    Grid, 
-    Box, 
-    AppBar, 
-    Toolbar, 
-    Container, 
-    TextField,
-    MenuItem,
-    Typography,
-    useTheme,
-  } from "@mui/material"
+  ShoppingCart as ShoppingCartIcon
+} from '@mui/icons-material';
 
-// Router
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  boxShadow: '1px',
+  backgroundColor: '#ffffff',
+  marginBottom: '20px'
+}));
 
-const Navbar = () => {
+const StyledToolBar = styled(Toolbar)(({ theme }) => ({
+    display:'flex'
+}));
 
-    return (
-     <>Navbar</>
-    )
+const StyledLogoBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  gap: '20px',
+  alignItems: 'center'
+}));
+
+const StyledLogoTypography = styled(Typography)(({ theme }) => ({
+  display: 'block',
+  fontSize: '18px',
+  fontWeight: 500,
+  color: '#000000',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '14px'
+  },
+}));
+
+export default function Navbar() {
+
+  return (
+    <StyledAppBar position="static">
+        <StyledToolBar>
+            <Link 
+              to='/' 
+              style={{
+                textDecoration: 'none'
+              }}
+            >
+              <StyledLogoBox>
+                 <img 
+                    src={'https://yetisplus.com/images/brand_logo.png'} 
+                    width={'40'} 
+                    height={'40'} 
+                    style={{
+                      height: '40px',
+                      objectFit: 'contain',
+                      borderRadius: '5px'
+                    }}
+                />
+                 <StyledLogoTypography>
+                   Ürün Kataloğu Case
+                </StyledLogoTypography>
+              </StyledLogoBox>
+            </Link>
+            <Box sx={{ flexGrow: 1 }} />
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="default"
+              sx={{ display: 'flex' }}
+            >
+              <Badge badgeContent={17} color="error">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+        </StyledToolBar>
+      </StyledAppBar>
+  );
 }
-
-export default Navbar
