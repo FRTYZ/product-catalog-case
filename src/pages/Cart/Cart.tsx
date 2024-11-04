982cd830b63494083e6db027bd71a96301d0cc5d
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-11-04 13:00:02 +0300

    Coverted Styled component for cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -6,7 +6,89 @@ import ShopCard from '../../components/Cards/ShopCard';
 import { useAppSelector } from '../../redux/store';
 
 // Material UI
-import { Container, Box, Grid2 as Grid, Typography } from '@mui/material'
+import { Container, Box, Grid2 as Grid, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+// Styled components
+const StyledHeadTypography = styled(Typography)(({ theme }) => ({
+    fontSize: '21px',
+    fontWeight: 600,
+    lineHeight: '26.25px',
+    marginBottom: '40px',
+    textAlign: 'left',
+    [theme.breakpoints.down('md')]: {
+        textAlign: 'center'
+      },
+}));
+
+const StyledSubTypography = styled(Typography)(({ theme }) => ({
+    fontSize: '16px',
+    color: 'gray',
+    textAlign: 'center',
+    fontWeight: 400
+}));
+
+const StyledCartBox = styled(Box)(({ theme }) => ({
+    display: 'none',
+    [theme.breakpoints.down('md')]: {
+        display: 'flex'
+    },
+    gap: '20px',
+    marginBottom: '40px',
+    textAlign: 'center',
+    justifyContent: 'center'
+}));
+
+const StyledRightBox = styled(Box)(({ theme }) => ({
+    py:4,
+    gap: 4,
+}));
+
+const StyledRightContentBox = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'space-between',
+    textAlign: 'left',
+    fontWeight: 400,
+    color: 'black'
+}));
+
+const StyledRightContentTypography = styled(Typography)(({ theme }) => ({
+    color: 'black', 
+    fontWeight: 600, 
+    fontSize: '18px'
+}));
+
+const StyledRightButtonBox = styled(Box)(({ theme }) => ({
+    display: 'grid',
+    marginTop: '40px'
+}));
+
+const StyledRightXButton = styled(XButton)(({ theme }) => ({
+    backgroundColor: '#17a77f',
+    color: 'white',
+    fontWeight: 600,
+    justifyContent: 'center',
+    textAlign: 'center',
+    width: '100%',
+}));
+
+const StyledNoResultCartBox = styled(Box)(({ theme }) => ({
+    display: 'block',
+    gap: '20px',
+    marginBottom: '40px',
+    textAlign: 'center',
+    justifyContent: 'center'
+}));
+
+const StyledNoResultXButton = styled(XButton)(({ theme }) => ({
+    marginTop: '40px',
+    backgroundColor: '#17a77f',
+    color: 'white',
+    fontWeight: 600,
+    justifyContent: 'center',
+    textAlign: 'center',
+}));
+
 
 function Cart() {
     // Get value in bag
@@ -30,164 +112,53 @@ function Cart() {
             {bagItems.length > 0 ? (
                 <Grid container spacing={8}>
                     <Grid size={{ xs: 12, lg: 8 }}>
-                        <Typography
-                            sx={{
-                                fontSize: '21px',
-                                fontWeight: 600,
-                                lineHeight: '26.25px',
-                                marginBottom: 4,
-                                textAlign: {xs: 'center', lg: 'left'}
-                            }}
-                        >
+                        <StyledHeadTypography>
                             Sepet
-                        </Typography>
-                        <Box
-                            sx={{
-                                display: {xs: 'flex', lg: 'none'},
-                                gap: 2,
-                                my: 4,
-                                textAlign: 'center',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Typography
-                                sx={{
-                                    fontSize: '16px',
-                                    color: 'gray',
-                                    textAlign: 'center',
-                                    fontWeight: 400
-                                }}
-                            >
-                                {bagItems.length} ürün
-                            </Typography>
+                        </StyledHeadTypography>
+                        <StyledCartBox>
+                            <StyledSubTypography> {bagItems.length} ürün </StyledSubTypography>
                             <Typography>|</Typography>
-                            <Typography
-                                sx={{
-                                    fontSize: '16px',
-                                    color: 'gray',
-                                    textAlign: 'center',
-                                    fontWeight: 400
-                                }}
-                            >
-                                ${roundedTotalPrice}
-                            </Typography>
-                        </Box>
+                            <StyledSubTypography> ${roundedTotalPrice}</StyledSubTypography>
+                        </StyledCartBox>
                         <ShopCard bagItems={bagItems} />
                     </Grid>
                     <Grid size={{ xs: 12, lg: 4 }}>
-                        <Typography
-                            sx={{
-                                fontSize: '21px',
-                                fontWeight: 600,
-                                lineHeight: '26.25px',
-                                marginBottom: 4,
-                                textAlign: {xs: 'center', lg: 'left'}
-                            }}
-                        >
+                        <StyledHeadTypography>
                             Özet
-                        </Typography>
-                        <Box
-                            sx={{
-                                py: 4,
-                                gap: 4
-                            }}
-                        >
-                            <Box
-                                sx={{
-                                    gap: 4
-                                }}
-                            >
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        justifyContent: 'space-between',
-                                        textAlign: 'left',
-                                        fontWeight: 400,
-                                        color: 'black'
-                                    }}
-                                >
-                                    <Typography>Ürün sayısı</Typography>
-                                    <Typography sx={{ color: 'black', fontWeight: 600, fontSize: '18px' }}>{bagItems.length}</Typography>
-                                </Box>
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        justifyContent: 'space-between',
-                                        textAlign: 'left',
-                                        fontWeight: 400,
-                                        color: 'black'
-                                    }}
-                                >
-                                    <Typography>Toplam</Typography>
-                                    <Typography sx={{ color: 'black', fontWeight: 600, fontSize: '18px' }}>{roundedTotalPrice} TL </Typography>
-                                </Box>
-                                <Box
-                                    sx={{
-                                        display: 'grid',
-                                        mt: 4
-                                    }}
-                                >
-                                    <XButton 
-                                        text='Ödeme İşlemine Geç' 
-                                        variant="contained" 
-                                        buttonSize="large"
-                                        sx={{ 
-                                            backgroundColor: '#17a77f',
-                                            color: 'white',
-                                            fontWeight: 600,
-                                            justifyContent: 'center',
-                                            textAlign: 'center',
-                                            width: '100%',
-                                        }}
-                                    />
-                                </Box>
-                            </Box>
-                        </Box>
+                        </StyledHeadTypography>
+                        <StyledRightBox>
+                            <StyledRightContentBox>
+                                <Typography>Ürün sayısı</Typography>
+                                <StyledRightContentTypography>{bagItems.length}</StyledRightContentTypography>
+                            </StyledRightContentBox>
+                            <StyledRightContentBox>
+                                <Typography>Toplam</Typography>
+                                <StyledRightContentTypography>{roundedTotalPrice} TL </StyledRightContentTypography>
+                            </StyledRightContentBox>
+                            <StyledRightButtonBox>
+                                <StyledRightXButton 
+                                    text='Ödeme İşlemine Geç' 
+                                    variant="contained" 
+                                    buttonSize="large"
+                                />
+                            </StyledRightButtonBox>
+                        </StyledRightBox>
                     </Grid>
                 </Grid>
             ): (
                 <>
-                <Typography
-                    sx={{
-                        fontSize: '21px',
-                        fontWeight: 600,
-                        lineHeight: '26.25px',
-                        marginBottom: 4,
-                        textAlign: {xs: 'center', lg: 'left'}
-                    }}
-                >
-                    Sepet
-                </Typography>
-                <Box
-                    sx={{
-                        display: 'block',
-                        gap: 2,
-                        my: 4,
-                        textAlign: 'center',
-                        justifyContent: 'center'
-                    }}
-                >
-                    <Typography
-                        sx={{
-                            fontSize: '18px'
-                        }}
-                    >Sepetinizde henüz ürün bulunmuyor. Dilerseniz ürün katalog sayfasına gidip, dilediğiniz ürünü seçebilirsiniz.</Typography>
-                    <XButton 
-                        text='Ürün katalog sayfası' 
-                        variant="contained" 
-                        buttonSize="large"
-                        href='/'
-                        sx={{ 
-                            mt: 4,
-                            backgroundColor: '#17a77f',
-                            color: 'white',
-                            fontWeight: 600,
-                            justifyContent: 'center',
-                            textAlign: 'center',
-                        }}
-                    />
-
-                </Box>
+                    <StyledHeadTypography>
+                        Sepet
+                    </StyledHeadTypography>
+                    <StyledNoResultCartBox>
+                        <StyledSubTypography>Sepetinizde henüz ürün bulunmuyor. Dilerseniz ürün katalog sayfasına gidip, dilediğiniz ürünü seçebilirsiniz.</StyledSubTypography>
+                        <StyledNoResultXButton 
+                            text='Ürün katalog sayfası' 
+                            variant="contained" 
+                            buttonSize="large"
+                            href='/'
+                        />
+                    </StyledNoResultCartBox>
                 </>
             )}
         </Container>
