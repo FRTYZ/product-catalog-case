44ed06b20220db7b39762c2d5e775e53e0cfb658
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-11-04 16:53:28 +0300

    Created unit test for navbar

---
+++ src/Layout/Navbar.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/store'; // Redux store'unuzun doğru yolu
+import Navbar from './Navbar'; // Navbar bileşeninin doğru yolu
+
+describe('Navbar Component', () => {
+    test('Navbar bileşeninin render olup olmadığını kontrol etme', () => {
+        render(
+            <Router>
+                <Provider store={store}>
+                    <Navbar />
+                </Provider>
+            </Router>
+        );
+
+        // Navbar bileşeninin içinde beklenen öğeleri kontrol edelim
+        expect(screen.getByText('Ürün Kataloğu Case')).toBeInTheDocument();
+        expect(screen.getByRole('img')).toHaveAttribute('src', 'https://yetisplus.com/images/brand_logo.png');
+    });
+
+    test('Sepet ikonunun render edilip edilmediğini kontrol etme', () => {
+        render(
+            <Router>
+                <Provider store={store}>
+                    <Navbar />
+                </Provider>
+            </Router>
+        );
+
+        // Sepet ikonunun render edildiğini kontrol et
+        const cartIcon = screen.getByLabelText(/show/i);
+        expect(cartIcon).toBeInTheDocument();
+    });
+});
