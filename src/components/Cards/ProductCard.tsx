5e0991dcec6f54c71d4cb39e015c3a3dd22f9787
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-11-04 04:43:36 +0300

    Added infinite scroll for product card

--- src/components/Cards/ProductCard.tsx
+++ src/components/Cards/ProductCard.tsx
@@ -34,10 +34,10 @@ const ProductCard = ({ data, grid }: AdCardProps) => {
     // useState
     const [cardData, setCardData] = useState<ProductsProps[]>(data!);
     const [pageNumber, setPageNumber] = useState<number>(1);
-    const [endPagination, setEndPagination] = useState<boolean>(false);
     const pageSize: number = 10;
     
     useEffect(() => {
+        setPageNumber(1)
         if(data && data.length > 0){
             setCardData(Array.isArray(data) ? data.slice(0, pageSize) : []);
         }
@@ -51,8 +51,6 @@ const ProductCard = ({ data, grid }: AdCardProps) => {
 
             if(limitedData.length > 0){
                 setCardData((prevData) => [ ...prevData, ...limitedData ]);
-            }else{
-                setEndPagination(true)
             }
         }
         if(pageNumber > 1){
@@ -60,13 +58,24 @@ const ProductCard = ({ data, grid }: AdCardProps) => {
         }
     }, [pageNumber])
 
-    const handlePagination = () => {
-        setPageNumber((prev) => prev + 1)
-    }
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 50) {
+                setPageNumber((prev) =>  prev + 1);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <Grid container spacing={2}>
             {data && data.length > 0 ? (
+
                 cardData.map((item, index) => (
                     <Grid size={{ lg: grid[0], md: grid[1], sm:grid[2], xs: grid[3]}} key={index}>
                         <Card sx={{
@@ -133,37 +142,6 @@ const ProductCard = ({ data, grid }: AdCardProps) => {
                             
                         }}
                     >Veri bulunamadı veya farklı filtreleme işlemi uygulayabilirsiniz.</Typography>
-                </Grid>
-            )}
-            {!endPagination && (data && data.length > 10) && (
-                <Grid size={{ xs: 12, lg: 12 }}>
-                    <Box sx={{
-                         textAlign: 'center', 
-                         marginTop: '30px'
-                    }}>
-                        <Button
-                            variant="outlined"
-                            sx={{
-                                    backgroundColor: '#25d6a2',
-                                    color: '#FFFFFF',
-                                    borderRadius: '50px',
-                                    border: '3px solid transparent',
-                                    textTransform: 'none',
-                                    marginRight: {xl: '20px', lg: '20px', md: '0px', sm:'0px', xs: '0px'}, 
-                                    marginBottom: '60px',
-                                    fontSize :'16',
-                                    fontWeight: 700,
-                                    '&:hover': { 
-                                        backgroundColor: '#FFFFFF', 
-                                        border: '3px solid #25d6a2', 
-                                        color: '#25d6a2' 
-                                    },
-                            }}
-                            onClick={() => handlePagination()}
-                        >
-                            Daha fazla yükle
-                        </Button>
-                    </Box>
                 </Grid>
             )}
         </Grid>
